a8e6663ca523957b1244900624a8bc3922481af0
vishnu <vishnuvichithran@gmail.com>
2025-07-26 20:39:07 +0530

    first changes

--- my_package/learning_python.py
+++ my_package/learning_python.py
@@ -1,5 +1,7 @@
-def sayhello(name):
-    return f"Hello, {name}!"
+def calculate_discounted_price(price,tax_percentage,discount_percentage):
+    tax_amount = price * ( tax_percentage / 100 )
+    price_withtax = price + tax_amount
+    discount_amount = price_withtax * ( discount_percentage / 100 )
+    final_price = price_withtax - discount_amount
 
-def add(a, b):
-    return a + b
+    return final_price
