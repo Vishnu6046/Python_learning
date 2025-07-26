43cc1978616102d103b3c56fabc8f5d0838a9b10
vishnu <vishnuvichithran@gmail.com>
2025-07-26 21:20:57 +0530

    first function to calculate discount

--- my_package/learning_python.py
+++ my_package/learning_python.py
@@ -1,7 +1,21 @@
 def calculate_discounted_price(price,tax_percentage,discount_percentage):
+    """
+    this function is used to calculate discounted price
+
+    Args:
+        price (int or float): actual price without tax
+        tax_percentage (int or float): GST or taxes to include
+        discount_percentage (int or float): total discount
+
+    Returns:
+        int or float: final price including tax
+    """
+ 
     tax_amount = price * ( tax_percentage / 100 )
     price_withtax = price + tax_amount
     discount_amount = price_withtax * ( discount_percentage / 100 )
     final_price = price_withtax - discount_amount
 
     return final_price
+#...............................................................................
+
--- outside_file.py
+++ outside_file.py
@@ -1,3 +1,14 @@
 from my_package import learning_python
 
+#this is to calculate discount using a function in another package.
+#this function works because it imported.
+
+price = float(input("Enter price of product: "))
+tax_percentage = 18
+discount_percentage = float(input("Enter discount percentage: "))
+
+final_price = learning_python.calculate_discounted_price(price,tax_percentage,discount_percentage)
+print("Final amount is: ", final_price)
+
+#----------------------------------------------------------------------------
 
