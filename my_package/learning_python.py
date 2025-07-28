45e33a3fd10f9bfbb7d6d267fd1d469f102fdd42
vishnu <vishnuvichithran@gmail.com>
2025-07-28 23:15:44 +0530

    exercise to list operation

--- my_package/learning_python.py
+++ my_package/learning_python.py
@@ -1,8 +1,7 @@
 def calculate_discounted_price(price,tax_percentage,discount_percentage):
-    """
-    this function is used to calculate discounted price
+    
+#this function is used to calculate discounted price
 
-    """ 
     tax_amount = price * ( tax_percentage / 100 )
     price_withtax = price + tax_amount
     discount_amount = price_withtax * ( discount_percentage / 100 )
@@ -12,10 +11,9 @@ def calculate_discounted_price(price,tax_percentage,discount_percentage):
 #...............................................................................
 
 def check_prime_number(num):
-    """
-    This function checks if a number prime or not
 
-    """
+#This function checks if a number prime or not
+
     if num > 1:
         for i in range(2,num):
             if num % i == 0:
@@ -27,4 +25,17 @@ def check_prime_number(num):
         print(num, "is not a prime number") 
 #--------------------------------------------------------------------------------
 
-       
+def calculate_BMI(height,weight):
+
+    bmi = weight / (height ** 2)
+    print(f"Your BMI is: {bmi:.2f}")
+
+    if bmi < 18.5:
+        print("You are underweight")
+    elif bmi < 25:
+        print("You have a normal weight")
+    elif bmi < 30:
+        print("You are overweight")
+    else:
+        print("You are obese")   
+#------------------------------------------------------------------------------------     
--- outside_file.py
+++ outside_file.py
@@ -17,3 +17,32 @@ from my_package import learning_python
 # learning_python.check_prime_number(num)
 # ---------------------------------------------------------------------------------         
 
+# height = float(input("Enter your height in meters: "))
+# weight = float(input("Enter your weight in kilograms: "))
+
+# learning_python.calculate_BMI(height,weight)       
+#----------------------------------------------------------------------------------
+
+# assignment_score = float(input("Enter score of assignment(out of 100) = "))
+# test_score = float(input("Enter score of test(out of 100) = "))
+
+# assignment_weight = 0.4
+# test_weight = 0.6
+
+# weighted_average = (assignment_score * assignment_weight) + (test_score * test_weight)
+
+# if weighted_average > 90:
+#     letter_grade = "A"
+# elif weighted_average > 80:
+#     letter_grade = "B"
+# elif weighted_average > 70:
+#     letter_grade = "C"
+# elif weighted_average > 60:
+#     letter_grade = "D"
+# elif weighted_average > 50:
+#     letter_grade = "E"
+# else:
+#     letter_grade = "F"
+
+# print("Your final grade is {} and letter grade is {}" .format(weighted_average,letter_grade)) 
+#----------------------------------------------------------------------------------------------------
